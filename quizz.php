3a719edc0bb2fa09a123c24f951bd8598c49f464
Hugo <humoreaux@stpbb.org>
2024-01-24 15:49:32 +0100

    Création des pages

---
+++ quizz.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz</title>
+    <style>
+        /* Add your CSS styles here */
+    </style>
+</head>
+<body>
+    <h1>Welcome to the Quiz!</h1>
+    <form action="quiz.php" method="post">
+        <!-- Add your quiz questions and options here -->
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
