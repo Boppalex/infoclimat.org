dfb9519c959295cfd76267a08f0a4050afb1842e
Hugo <humoreaux@stpbb.org>
2024-02-01 16:45:20 +0100

    Style de backend modif

--- backend.php
+++ backend.php
@@ -29,101 +29,240 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <title>CRUD admin</title>
     <style>
-       .btn {
-  --color2: #055634;
-  --color1: grey;
-  perspective: 1000px;
-  padding: 1em 1em;
-  background: linear-gradient(var(--color1), var(--color2));
-  border: none;
-  outline: none;
-  font-size: 20px;
-  text-transform: uppercase;
-  letter-spacing: 4px;
-  color: #fff;
-  text-shadow: 0 10px 10px #000;
-  cursor: pointer;
-  transform: rotateX(70deg) rotateZ(30deg);
-  transform-style: preserve-3d;
-  transition: transform 0.5s;
-}
+        footer {
+            background-color: #055634;
+            color: white;
+        }
 
-.btn::before {
-  content: "";
-  width: 100%;
-  height: 15px;
-  background-color: var(--color2);
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  transform: rotateX(90deg);
-  transform-origin: bottom;
-}
+        .superposition-simple {
+            position: relative;
+            width: 100%;
+        }
 
-.btn::after {
-  content: "";
-  width: 15px;
-  height: 100%;
-  background-color: var(--color1);
-  position: absolute;
-  top: 0;
-  right: 0;
-  transform: rotateY(-90deg);
-  transform-origin: right;
-}
+        .superposition-simple .image-originale {
+            display: block;
+            width: 100%;
+            height: auto;
+        }
 
-.btn:hover {
-  transform: rotateX(30deg) rotateZ(0);
-}
+        .superposition-simple .texte-original {
+            color: #fff;
+            font-size: 20px;
+            line-height: 1.5em;
+            text-shadow: 2px 2px 2px #000;
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 100%;
+        }
 
+        .superposition-simple .texte-hover {
+            position: absolute;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            opacity: 0;
+            transition: .5s ease;
+        }
+
+        .superposition-simple:hover .texte-normal {
+            opacity: 0;
+        }
+
+        .superposition-simple:hover .texte-hover {
+            opacity: 1;
+        }
+
+        .superposition-simple .texte-normal {
+            transition: .5s ease;
+        }
+
+        .btn {
+            --color2: #055634;
+            --color1: grey;
+            perspective: 1000px;
+            padding: 1em 1em;
+            background: linear-gradient(var(--color1), var(--color2));
+            border: none;
+            outline: none;
+            font-size: 20px;
+            text-transform: uppercase;
+            letter-spacing: 4px;
+            color: #fff;
+            text-shadow: 0 10px 10px #000;
+            cursor: pointer;
+            transform: rotateX(70deg) rotateZ(30deg);
+            transform-style: preserve-3d;
+            transition: transform 0.5s;
+        }
+
+        .btn::before {
+            content: "";
+            width: 100%;
+            height: 15px;
+            background-color: var(--color2);
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            transform: rotateX(90deg);
+            transform-origin: bottom;
+        }
+
+        .btn::after {
+            content: "";
+            width: 15px;
+            height: 100%;
+            background-color: var(--color1);
+            position: absolute;
+            top: 0;
+            right: 0;
+            transform: rotateY(-90deg);
+            transform-origin: right;
+        }
+
+        .btn:hover {
+            transform: rotateX(30deg) rotateZ(0);
+        }
     </style>
 </head>
+<header
+    class="entete flex flex-col sm:flex-row justify-center items-center p-1 sm:p-8 md:p-16 lg:p-20 xl:p-24 bg-cover bg-center h-300 sm:h-200 md:h-250 lg:h-300 xl:h-500"
+    style="background-image: url('Images/wave.png');">
+    <a href="accueil.php" class="mb-2 sm:mb-0 sm:mr-2">
+        <img src="images/terre1.jpg" alt="logo" class="w-32 h-32">
+    </a>
 
-<body class="bg-gray-100">
-    <h2>Welcome, <?php echo $_SESSION["username"]; ?>!</h2>
+    <!-- Menu burger Bootstrap -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <div class=" rounded-full w-full sm:w-20 h-20 text-center flex mb-2 sm:mb-0 sm:mr-2">
+                        <div class="superposition-simple  "><a href="accueil.php">
+                                <div class="texte-normal ">
+                                    <div class="texte-original ">Accueil</div>
+                                </div>
+                                <div class="texte-hover "><img decoding="async" class="image-originale "
+                                        src="Images/feuille.png" />
+                                    <div class="texte-original">Accueil</div>
+                                </div>
+                            </a></div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <div class=" rounded-full w-full sm:w-20 h-20 text-center flex mb-2 sm:mb-0 sm:mr-2">
+                        <div class="superposition-simple "><a href="blog.php">
+                                <div class="texte-normal ">
+                                    <div class="texte-original">Blog</div>
+                                </div>
+                                <div class="texte-hover "><img decoding="async" class="image-originale "
+                                        src="Images/nuage.png" />
+                                    <div class="texte-original">Blog</div>
+                                </div>
+                            </a></div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <div class=" rounded-full w-full sm:w-20 h-20 text-center flex mb-2 sm:mb-0 sm:mr-2">
+                        <div class="superposition-simple "><a href="quizz.php">
+                                <div class="texte-normal ">
+                                    <div class="texte-original">Quizz</div>
+                                </div>
+                                <div class="texte-hover "><img decoding="async" class="image-originale "
+                                        src="Images/soleil.png" />
+                                    <div class="texte-original">Quizz</div>
+                                </div>
+                            </a></div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <div class=" rounded-full w-full sm:w-20 h-20 text-center flex">
+                        <div class="superposition-simple "><a href="apropos.php">
+                                <div class="texte-normal ">
+                                    <div class="texte-original">À propos</div>
+                                </div>
+                                <div class="texte-hover "><img decoding="async" class="image-originale "
+                                        src="Images/glace.png" />
+                                    <div class="texte-original">À propos</div>
+                                </div>
+                            </a></div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
 
-    <table>
-        <thead>
-            <th>ID</th>
-            <th>Titre</th>
-            <th>Description</th>
-            <th>Article</th>
-            <th>Statut</th>
+<body class="bg-gray-100 ">
+    <div class="container">
+        <h1 class="text-3xl" style="text-align: center; margin-top:50px;">Welcome,
+            <?php echo $_SESSION["username"]; ?>!
+        </h1>
 
-        </thead>
-        <tbody>
-            <?php
-            foreach ($result as $carte) {
-            ?>
-                <tr>
-                    <td><?= $carte['id'] ?></td>
-                    <td><?= $carte['titre'] ?></td>
-                    <td><?= $carte['description'] ?></td>
-                    <td><?= $carte['article'] ?></td>
-                    <td><?= $carte['statut'] ?></td>
-                    <td>
-                    </br> 
-                        <a href="details.php?id=<?= $carte['id'] ?>">Voir</a>
-                        </br>
-                        <a href="edit.php?id=<?= $carte['id'] ?>">Modifier</a>
-                        <a href="delete.php?id=<?= $carte['id'] ?>">Supprimer</a>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+        <table>
+            <thead>
+                <th>ID</th>
+                <th>Titre</th>
+                <th>Description</th>
+                <th>Article</th>
+                <th>Statut</th>
 
-    
-    <button class="btn"><a href="add.php">Ajouter</a></button>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($result as $carte) {
+                    ?>
+                    <tr>
+                        <td>
+                            <?= $carte['id'] ?>
+                        </td>
+                        <td>
+                            <?= $carte['titre'] ?>
+                        </td>
+                        <td>
+                            <?= $carte['description'] ?>
+                        </td>
+                        <td>
+                            <?= $carte['article'] ?>
+                        </td>
+                        <td>
+                            <?= $carte['statut'] ?>
+                        </td>
+                        <td>
+                            </br>
+                            <a href="edit.php?id=<?= $carte['id'] ?>">Modifier</a>
+                            <a href="delete.php?id=<?= $carte['id'] ?>">Supprimer</a>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </tbody>
+        </table>
 
+        <a href="add.php" class="text-white">
+        <button class="btn">Ajouter
+        </button>
+        </a>
 
-    <footer class="bg-green-600 p-4 w-full fixed bottom-0">
+    </div>
+    <footer class="p-4 w-full">
         <p class="flex justify-center">@SIO2Groupe2</p>
-        <p class="flex justify-center">By Adrien Cirade, Roman Bourguignon, Steven Thomassin, Alexandre Bopp, Samuel Azoulay, Hugo Moreaux</p>
+        <p class="flex justify-center">By Adrien Cirade, Roman Bourguignon, Steven Thomassin, Alexandre Bopp, Samuel
+            Azoulay, Hugo Moreaux</p>
     </footer>
 </body>
 
